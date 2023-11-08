066e744075e5696c5a420e8db5daddc03a8ef686
hamed <hamedpooramiri1007@gmail.com>
2023-11-08 17:17:31 +0330

    finish implementing sample usage of Analytics Modules

--- Analytics-architecture/ViewController.swift
+++ Analytics-architecture/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import Feature_Module
 
 class ViewController: UIViewController {
+
+    var analyticsService: AnalyticsService?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        analyticsService?.log(event: ViewControllerEvents.viewAppeared())
+    }
 }
 
+struct ViewControllerEvents: AnalyticsEvent {
+    var name: String
+    var metadata: [String : String]
+    
+    init(name: String, metadata: [String : String]) {
+        self.name = name
+        self.metadata = metadata
+    }
+    
+    static func viewAppeared() -> Self {
+        ViewControllerEvents(name: "viewAppeared", metadata: [:])
+    }
+}
