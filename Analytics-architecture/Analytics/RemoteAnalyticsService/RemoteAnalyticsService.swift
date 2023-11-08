c08763f0803149dddd015b646c80d062ce87b76a
hamed <hamedpooramiri1007@gmail.com>
2023-11-08 16:37:17 +0330

    complete implementing RemoteAnalyticsService

--- Analytics-architecture/Analytics/RemoteAnalyticsService/RemoteAnalyticsService.swift
+++ Analytics-architecture/Analytics/RemoteAnalyticsService/RemoteAnalyticsService.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+/// Provide application logic here or communicate with domain layer to satisfy business logic
+
+public class RemoteAnalyticsService {
+    
+    private let analyticsEngine: AnalyticsEngine
+    
+    init(analyticsEngine: AnalyticsEngine) {
+        self.analyticsEngine = analyticsEngine
+    }
+    
+}
+
+extension RemoteAnalyticsService: AnalyticsService {
+    func log(event: AnalyticsEvent) {
+        analyticsEngine.send(name: event.name, metadata: event.metadata)
+    }
+}
